4fe7b89ec2de2c4b58206740168a334e9d83c742
Ruben Lorenz Y. <150222307+rulo-codes@users.noreply.github.com>
2025-07-18 03:21:16 +0000

    Added 2 animations examples

--- script.js
+++ script.js
@@ -18,4 +18,18 @@ animate('span', {
   loop: true
 });
 
+
+animate(['feTurbulence', 'feDisplacementMap'], {
+  baseFrequency: .05,
+  scale: 15,
+  alternate: true,
+  loop: true
+});
+
+animate('polygon', {
+  points: '64 68.64 8.574 100 63.446 67.68 64 4 64.554 67.68 119.426 100',
+  alternate: true,
+  loop: true
+});
+
 console.log("ItMoves Test")
